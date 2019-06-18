ef4a42e23b7f5b1120a9362ca9e09ad8645255d2
rupeshchamling <rupeshrakhamicha@gmail.com>
2019-06-18 21:42:27 +0545

    single page changes

--- js/global.js
+++ js/global.js
@@ -1,6 +1,17 @@
-// added class on dropdown menu span
-
 jQuery(document).ready(function($) {
+    // adding class fixed in navigation bar
+
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        if (height > 300) {
+            $('.main-navigation, #up-btn').addClass('fixed');
+        } else {
+            $('.main-navigation, #up-btn').removeClass('fixed');
+        }
+    });
+
+    // added class on dropdown menu span
+
     if ($(document).width() < 1200) {
         var $menu_item = $('.menu-item-has-children');
 
@@ -10,10 +21,10 @@ jQuery(document).ready(function($) {
             $(this).parent().toggleClass('menu-open').siblings().removeClass('menu-open');
         });
     }
-});
 
-// To top Java Script
+    // To top Java Script
 
-jQuery(document).ready(function($) {
-
+    $('#up-btn').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 1000);
+    });
 });
--- single.php
+++ single.php
@@ -14,27 +14,70 @@ get_header();
 	<div class="row">
 		<main id="main" class="col-md-12">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+			<?php
+			while (have_posts()) :
+				the_post();
 
-			get_template_part( 'template-parts/content', 'single' );
+				get_template_part('template-parts/content', 'single');
 
-			the_post_navigation(
-				array(
-					'prev_text'          => __( '<i class="fas fa-chevron-left"></i>  %title', 'wildtravel' ),
-					'next_text'          => __( '%title <i class="fas fa-chevron-right"></i>', 'wildtravel' ),
-					'screen_reader_text' => ' ',
-				)
-			);
+				the_post_navigation(
+					array(
+						'prev_text'          => __('<i class="fas fa-chevron-left"></i>  %title', 'wildtravel'),
+						'next_text'          => __('%title <i class="fas fa-chevron-right"></i>', 'wildtravel'),
+						'screen_reader_text' => ' ',
+					)
+				);
+				?>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				<?php
+				// Query random posts.
+				$the_query = new WP_Query(
+					array(
+						'post_type'           => 'post',
+						'orderby'             => 'DESC',
+						'ignore_sticky_posts' => 1,
+						'posts_per_page'      => 5,
+					)
+				);
+				?>
+				<?php
+				// If we have posts lets show them.
+				if ($the_query->have_posts()) :
+					?>
+					<div class="related_wrap">
+						<h2 class="section-title"><span><?php esc_html_e('Related Posts', 'text_domain'); ?></span></h2>
 
-		endwhile; // End of the loop.
-		?>
+						<?php
+						// Loop through the posts.
+						while ($the_query->have_posts()) :
+							$the_query->the_post();
+							?>
+							<article class="row">
+								<div class="col-sm-5 figure">
+									<a href="<?php the_permalink(); ?>">
+										<?php wildtravel_post_thumbnail(); ?>
+									</a>
+								</div>
+
+								<div class="blog_content col-sm-7">
+									<h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+								</div>
+							</article>
+
+						<?php endwhile; ?>
+						<?php wp_reset_postdata(); ?>
+					</div>
+				<?php endif; ?>
+
+				<?php
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if (comments_open() || get_comments_number()) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
 
 		</main><!-- #main -->
 	</div><!-- .row -->
